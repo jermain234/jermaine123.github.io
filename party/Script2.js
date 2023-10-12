cc989426d4a1990f0ba92fb3b15143bc111fe57b
jermain234 <147757848+jermain234@users.noreply.github.com>
2023-10-12 22:00:08 +0200

    Add files via upload

---
+++ party/Script2.js
@@ -0,0 +1,14 @@
+var now = new Date().getTime();
+
+
+var x = setInterval(function () {
+    var currentTime = new Date().getTime();
+    var distance = currentTime - now;
+
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    document.getElementById("countdown").innerHTML = days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
+}, 1000);
